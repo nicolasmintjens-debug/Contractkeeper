3397358ebcca274505e5178fef0c4819e4912d58
NMI <nicolasmintjens@gmail.com>
2026-07-29 17:11:19 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,40 +1,43 @@
-/* ===========================================
+/* ==========================================
    ContractKeeper
-   modal.js
-=========================================== */
+   Modal
+========================================== */
 
-let modal = null;
-let addButton = null;
+let modal;
+let addButton;
+let modalContent;
 
 function initModal() {
 
-    console.log("initModal uitgevoerd");
-
     modal = document.getElementById("addContractModal");
     addButton = document.getElementById("nav-add");
+    modalContent = modal.querySelector(".modal-content");
 
-    console.log(modal);
-    console.log(addButton);
-
-    if (!modal || !addButton) {
-        console.error("Modal of knop niet gevonden.");
+    if (!modal || !addButton || !modalContent) {
+        console.error("Modal niet correct gevonden.");
         return;
     }
 
     addButton.addEventListener("click", openModal);
 
     modal.addEventListener("click", function (event) {
+
         if (event.target === modal) {
             closeModal();
         }
+
     });
 
 }
 
 function openModal() {
+
     modal.classList.add("show");
+
 }
 
 function closeModal() {
+
     modal.classList.remove("show");
+
 }
