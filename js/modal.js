24c9e837fd85c1e2d99215954773ef03dbfa8ec1
NMI <nicolasmintjens@gmail.com>
2026-07-29 16:57:24 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,58 @@
+/* ===========================================
+   ContractKeeper
+   modal.js
+=========================================== */
 
+let modal = null;
+let addButton = null;
+
+/* ===========================================
+   Modal initialiseren
+=========================================== */
+
+function initModal() {
+
+    modal = document.getElementById("addContractModal");
+    addButton = document.getElementById("nav-add");
+
+    if (!modal) {
+        console.error("Modal niet gevonden.");
+        return;
+    }
+
+    if (!addButton) {
+        console.error("Plusknop niet gevonden.");
+        return;
+    }
+
+    addButton.addEventListener("click", openModal);
+
+    modal.addEventListener("click", function (event) {
+
+        if (event.target === modal) {
+            closeModal();
+        }
+
+    });
+
+}
+
+/* ===========================================
+   Open
+=========================================== */
+
+function openModal() {
+
+    modal.classList.add("show");
+
+}
+
+/* ===========================================
+   Sluiten
+=========================================== */
+
+function closeModal() {
+
+    modal.classList.remove("show");
+
+}
