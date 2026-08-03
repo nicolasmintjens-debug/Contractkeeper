3f30475d9b55bbd99d436780183dfe7fa7af83d3
NMI <nicolasmintjens@gmail.com>
2026-08-03 21:39:44 +0200

    Update services.js

--- js/services.js
+++ js/services.js
@@ -1,1 +1,25 @@
+/* ======================================================
+   BEKENDE DIENSTEN
+====================================================== */
 
+const SERVICES = [
+
+    {
+        name: "Netflix",
+        logo: "netflix.png",
+        category: "Streaming"
+    },
+
+    {
+        name: "Spotify",
+        logo: "spotify.webp",
+        category: "Muziek"
+    },
+
+    {
+        name: "Proximus",
+        logo: "proximus.png",
+        category: "Internet"
+    }
+
+];
